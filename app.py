f6ea698bb4e12389c160aa49dfcd881ec7c19ab0
abdulkadir <abdulkadir@itemci.com>
2022-11-24 18:24:14 +0300

    fix_some_err

--- app.py
+++ app.py
@@ -36,23 +36,23 @@ def get_all_currency(date):
     EUR.append(list2[1])
     USD.append(list1[2])
     USD.append(list2[2])
-    usd=min(USD)
-    tl=min(TRY)
-    eur=min(EUR)
+    usd=1/min(USD)
+    tl=1/min(TRY)
+    eur=1/min(EUR)
     return [tl,usd,eur]
 
 @app.route("/",methods = ["GET","POST"])
 def index():
     zipList=""
-    # name_curr=["TL","USD","EUR"]
-    # date=""
-    # result=get_all_currency(date)
-    # zipList=zip(name_curr,result)
-    # if request.method == "POST":
-    #     date=request.form['date']
-    #     result=get_all_currency(date)
-    #     zipList_date=zip(name_curr,result)
-    #     return render_template("index.html",zipList_date=zipList_date,zipList=zipList)
+    name_curr=["TL","USD","EUR"]
+    date=""
+    result=get_all_currency(date)
+    zipList=zip(name_curr,result)
+    if request.method == "POST":
+        date=request.form['date']
+        result=get_all_currency(date)
+        zipList_date=zip(name_curr,result)
+        return render_template("index.html",zipList_date=zipList_date,zipList=zipList)
     return render_template("index.html",zipList=zipList)
 
 if __name__ == "__main__":
