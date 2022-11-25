51bdc82b50175155f3bac782e82e1a0c3c41e701
abdulkadir <abdulkadir@itemci.com>
2022-11-25 12:27:58 +0300

    env file config done

--- app.py
+++ app.py
@@ -5,24 +5,22 @@ from dotenv import load_dotenv
 load_dotenv() 
 app = Flask(__name__)
 
-base="TRY"
-
 def get_date_currency_api_1(date):
     if date == "":
-        response = requests.request("GET", f"https://api.apilayer.com/exchangerates_data/latest?symbols=&base={base}", headers={"apikey": "DLDK3ipbf0Lo85OVjj5pchPyDfLk1HOS"}, data = {})
+        response = requests.request("GET", "{}/latest?symbols=&base={}".format(os.getenv("BASE_URL_API_1"),os.getenv("BASE_CURRENCY")), headers={"apikey":os.getenv("API_KEY_1")}, data = {})
     else:
-        response = requests.request("GET", f"https://api.apilayer.com/exchangerates_data/{date}?symbols=EUR%2CUSD%2CTRY&base={base}", headers={"apikey": "DLDK3ipbf0Lo85OVjj5pchPyDfLk1HOS"}, data = {})
+        response = requests.request("GET", "{}/{}?symbols=EUR%2CUSD%2CTRY&base={}".format(os.getenv("BASE_URL_API_1"),date,os.getenv("BASE_CURRENCY")), headers={"apikey":os.getenv("API_KEY_1")}, data = {})
     result = response.json()
     currency_list=[result["rates"]["TRY"],result["rates"]["EUR"],result["rates"]["USD"]]
     return currency_list
 
 def get_date_currency_api_2(date):
     if date == "":   
-        response = requests.request("GET", f"https://api.freecurrencyapi.com/v1/latest?apikey=1zlJTNqgEBcVtQQsZfcFslbgln3CE0KzpAykQL94&base_currency={base}", headers={"apikey": "1zlJTNqgEBcVtQQsZfcFslbgln3CE0KzpAykQL94"}, data = {})
+        response = requests.request("GET","{}latest?apikey={}&base_currency={}".format(os.getenv("BASE_URL_API_2"),os.getenv("API_KEY_2"),os.getenv("BASE_CURRENCY")), headers={"apikey":os.getenv("API_KEY_2")}, data = {})
         result = response.json()
         currency_list=[result["data"]["TRY"],result["data"]["EUR"],result["data"]["USD"]]
     else:
-        response = requests.request("GET", f"https://api.freecurrencyapi.com/v1/historical?apikey=1zlJTNqgEBcVtQQsZfcFslbgln3CE0KzpAykQL94&date_from={date}&date_to={date}&base_currency={base}", headers={"apikey": "1zlJTNqgEBcVtQQsZfcFslbgln3CE0KzpAykQL94"}, data = {})
+        response = requests.request("GET", "{}historical?apikey={}&date_from={}&date_to={}&base_currency={}".format(os.getenv("BASE_URL_API_2"),os.getenv("API_KEY_2"),date,date,os.getenv("BASE_CURRENCY")), headers={"apikey":os.getenv("API_KEY_2")}, data = {})
         result = response.json()
         currency_list=[result["data"][date]["TRY"],result["data"][date]["EUR"],result["data"][date]["USD"]]
     return currency_list
@@ -44,31 +42,17 @@ def get_all_currency(date):
     eur=1/min(EUR)
     return [tl,usd,eur]
 
-# @app.route("/",methods = ["GET","POST"])
-# def index():
-
-#     zipList=""
-#     name_curr=["TL","USD","EUR"]
-#     date=""
-#     result=get_all_currency(date)
-#     zipList=zip(name_curr,result)
-#     if request.method == "POST":
-#         date=request.form['date']
-#         result=get_all_currency(date)
-#         zipList_date=zip(name_curr,result)
-#         return render_template("index.html",zipList_date=zipList_date,zipList=zipList)
-#     return render_template("index.html",zipList=zipList)
-
-
 @app.route("/",methods = ["GET","POST"])
 def index():
-    
-    zipList= os.getenv("BASE_URL_API_1")
-    zipList2= os.getenv("BASE_URL_API_2")
-    print(zipList)
-    print(zipList2)
-    return render_template("index.html")
-
+    name_curr=["TL","USD","EUR"]
+    result=get_all_currency(date="")
+    zipList=zip(name_curr,result)
+    if request.method == "POST":
+        date=request.form['date']
+        result=get_all_currency(date)
+        zipList_date=zip(name_curr,result)
+        return render_template("index.html",zipList_date=zipList_date,zipList=zipList,base_curr=os.getenv("BASE_CURRENCY"))
+    return render_template("index.html",zipList=zipList,base_curr=os.getenv("BASE_CURRENCY"))
 
 if __name__ == "__main__":
     app.run(debug=True)
